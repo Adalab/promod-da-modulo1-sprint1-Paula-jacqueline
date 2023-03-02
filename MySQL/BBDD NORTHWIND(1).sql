901975c8a8b06afc0c49613e9e29eb080a4e4472
Paula <paulavelascosuarez6@gmail.comgit config --global user.email paulavelascosuarez6@gmail.com>
2023-03-02 11:46:53 +0000

    ejercicio consultas basicas 1 sql terminado

---
+++ MySQL/BBDD NORTHWIND(1).sql
@@ -0,0 +1,33 @@
+USE northwind;
+
+SELECT employee_id, last_name, first_name 
+FROM employees;
+
+SELECT * 
+FROM products 
+WHERE unit_price <= 5;
+
+SELECT *
+FROM products
+WHERE unit_price = 18 OR unit_price = 19 OR unit_price = 20;
+
+SELECT *
+FROM products
+WHERE unit_price >=15 AND unit_price <=50 ;
+
+
+SELECT *
+FROM products
+WHERE unit_price IS NULL ;
+
+SELECT *
+FROM products
+WHERE unit_price <15 AND product_id <10;
+
+SELECT *
+FROM products
+WHERE unit_price >15 AND product_id >10;
+
+SELECT ship_country
+FROM orders;
+
